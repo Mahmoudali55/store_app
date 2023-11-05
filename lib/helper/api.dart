6cd63c5f950e4db1c24aac2cf0bb5ffd8fcf8aff
mahmoudali15 <130462575+mahmoudali15@users.noreply.github.com>
2023-11-05 16:45:28 +0200

    create_put_api

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
-    http.Response response = await http.get(Uri.parse(url));
+  Future<dynamic> get({required String url, @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.get(Uri.parse(url), headers: headers);
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
@@ -19,6 +23,30 @@ class Api {
       @required dynamic bady,
       @required String? token}) async {
     Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: bady,
+      headers: headers,
+    );
+    print(response.body);
+    if (response == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'there is a Product with status code ${response.statusCode}with body ${response.body}');
+    }
+  }
+
+  Future<dynamic> put(
+      {required String url,
+      @required dynamic bady,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
     if (token != null) {
       headers.addAll({'Authorization': 'Bearer $token'});
     }
