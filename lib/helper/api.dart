909bfa1b6161eceb760f5b9874016ff7e9b4eaa5
mahmoudali15 <130462575+mahmoudali15@users.noreply.github.com>
2023-11-05 15:49:56 +0200

    add_post_api

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -11,5 +12,23 @@ class Api {
       throw Exception(
           'there is a Product with status code ${response.statusCode}');
     }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic bady,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: bady,
+      headers: headers,
+    );
+    print(response.body);
+    Map<String, dynamic> data = jsonDecode(response.body);
+    return data;
   }
 }
